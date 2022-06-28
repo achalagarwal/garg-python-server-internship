32dec788a068c5fe19685e61f2bd1b8deb4212d2
Achal Agarwal <achalagarwal.01@gmail.com>
2022-06-28 12:31:08 +0530

    final touches

--- app/main.py
+++ app/main.py
@@ -65,6 +65,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
     )
 
+
 admin = Admin(app, async_engine)
 
 
